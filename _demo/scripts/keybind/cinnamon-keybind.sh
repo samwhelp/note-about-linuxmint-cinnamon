5954cb65f683af6e1bd9784901300c8210fe5622
samwhelp <samwhelp@users.noreply.github.com>
2025-01-23 10:06:08 +0000

    fix

--- _demo/scripts/keybind/cinnamon-keybind.sh
+++ _demo/scripts/keybind/cinnamon-keybind.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 
+##
+## ## Link
+##
+## * https://github.com/samwhelp/note-about-linuxmint-cinnamon/blob/gh-pages/_demo/scripts/keybind/cinnamon-keybind.sh
+## * https://raw.githubusercontent.com/samwhelp/note-about-linuxmint-cinnamon/refs/heads/gh-pages/_demo/scripts/keybind/cinnamon-keybind.sh
+##
+
 echo test
